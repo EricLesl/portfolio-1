7474e9ec9835ac709b3386baea3b7cc41a668a41
ericlesl <eric.leslie@bauer.com>
2024-02-09 17:17:55 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Lily-Tweddle-Portfolio';
+  @ViewChild('mainContent') mainContent: ElementRef;
+
+  constructor(private renderer: Renderer2) { }
+  
+  ngAfterViewInit() {
+    this.doResize();
+    
+    this.renderer.listen(this.mainContent.nativeElement, 'scroll', () => {
+      this.doResize();
+    });
+  }
+
+  doResize(){
+    const sections: NodeListOf<HTMLElement> = this.mainContent.nativeElement.querySelectorAll('.content-section');
+    const navItems: NodeListOf<HTMLElement> = document.querySelectorAll('.nav-item');
+    const mainContentScrollTop = this.mainContent.nativeElement.scrollTop;
+    const mainContentHeight = this.mainContent.nativeElement.clientHeight;
+
+    sections.forEach(section => {
+      const sectionHeight = section.offsetHeight;
+      const mainContentHeight = this.mainContent.nativeElement.clientHeight;
+      const sectionTop = section.offsetTop - this.mainContent.nativeElement.scrollTop;
+      const sectionBottom = sectionTop + sectionHeight;
+      
+      // Calculate the visibility percentage
+      let visibleHeight = Math.min(mainContentHeight, sectionBottom) - Math.max(0, sectionTop);
+      let visibilityPercentage = (visibleHeight / mainContentHeight) * 100;
+
+      // Find the corresponding nav item
+      const targetNavItem = Array.from(navItems).find(item => item.getAttribute('data-target') === section.getAttribute('id'));
+
+      if (visibilityPercentage >= 50) {
+        // Increase nav item height by 100px if section takes up 50% or more of the view
+        this.renderer.setStyle(targetNavItem, 'height', '204px'); // Assuming original height is 104px
+      } else if ((sectionBottom) <= 0) {
+        // If the section has been scrolled past (its bottom is above the viewable area), set nav item height to 50% of its original height
+        this.renderer.setStyle(targetNavItem, 'height', '52px'); // 50% of the original height (assuming original is 104px)
+      } else {
+        // Reset to original height otherwise
+        this.renderer.setStyle(targetNavItem, 'height', '104px');
+      }
+    });
+  }
+
+  scrollToSection(sectionId: string) {
+    const sectionElement = this.mainContent.nativeElement.querySelector(`#${sectionId}`);
+    if (sectionElement) {
+      sectionElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
